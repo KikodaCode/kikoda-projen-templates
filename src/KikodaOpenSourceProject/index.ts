c7e134558981aa1a693674f958bff1fe3b1db678
Nathan Cazell <nathan.cazell@kikoda.com>
2022-05-31 15:05:05 +0000

    feat: replace any existing readme template with our own.

--- src/KikodaOpenSourceProject/index.ts
+++ src/KikodaOpenSourceProject/index.ts
@@ -1,5 +1,4 @@
-import { existsSync } from 'fs';
-import { Component, TextFile } from 'projen';
+import { Component, SampleReadme, TextFile } from 'projen';
 import { ArrowParens, EndOfLine, NodeProject, TrailingComma } from 'projen/lib/javascript';
 
 /**
@@ -326,54 +325,55 @@ export class KikodaOpenSourceProject<T extends NodeProject> extends Component {
       ],
     });
 
-    if (!existsSync(SupportingFiles.README) && !project.tryFindFile(SupportingFiles.README)) {
-      new TextFile(project, SupportingFiles.README, {
-        marker: false,
-        readonly: false,
-        lines: [
-          '[<img src="https://kikoda.com/wp-content/uploads/2019/07/Logo_White_bg.svg" width="300"/>](https://kikoda.com)',
-          `# ${options.title}`,
-          '',
-          `[![NPM](https://img.shields.io/npm/v/${project.package.packageName}?color=39a356)](https://www.npmjs.com/package/${project.package.packageName})`,
-          `[![License](https://img.shields.io/badge/license-Apache--2.0-blue)](${project.package.manifest.repository.url}/blob/main/LICENSE)`,
-          '',
-          `Use this ${options.title} to architect and model modern applications`,
-          '',
-          '## Install from NPM:',
-          '```',
-          `yarn add --dev ${project.package.packageName}`,
-          '',
-          '# or',
-          '',
-          `npm install ${project.package.packageName} --save-dev`,
-          '```',
-          '',
-          '## Usage',
-          '<!-- TODO: add Usage & Examples -->',
-          '',
-          '## Opening Issues',
-          '',
-          'If you encounter a bug with this package, we want to hear about it. Before opening a new issue, search the existing issues to avoid duplicates.',
-          '',
-          'When opening an issue, include the Kikoda Construct Library version, Node version, and stack trace if available. In addition, include the steps to reproduce when appropriate.',
-          '',
-          'You can also open an issue for a feature request.',
-          '',
-          '## Contributing',
-          '',
-          'If you find an issue with this package and have a fix, please feel free to open a pull request following the [procedures](CONTRIBUTING.md).',
-          '',
-          '## Testing',
-          '',
-          'If you contribute to this package you can run the tests using `yarn test`.',
-          '',
-          '## License',
-          '',
-          'Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.',
-          '',
-          'This product includes software developed at Kikoda (https://www.kikoda.com). Copyright 2022 Kikoda, LLC.',
-        ],
-      });
-    }
+    // if there is an existing SampleReadme, don't synthesize it.. we'll replace this with ours.
+    const existingReadme = project.components.find(c => c instanceof SampleReadme) as SampleReadme;
+    existingReadme.synthesize = () => {};
+
+    new SampleReadme(project, {
+      filename: SupportingFiles.README,
+      contents: [
+        '[<img src="https://kikoda.com/wp-content/uploads/2019/07/Logo_White_bg.svg" width="300"/>](https://kikoda.com)',
+        `# ${options.title}`,
+        '',
+        `[![NPM](https://img.shields.io/npm/v/${project.package.packageName}?color=39a356)](https://www.npmjs.com/package/${project.package.packageName})`,
+        `[![License](https://img.shields.io/badge/license-Apache--2.0-blue)](${project.package.manifest.repository.url}/blob/main/LICENSE)`,
+        '',
+        `Use this ${options.title} to architect and model modern applications`,
+        '',
+        '## Install from NPM:',
+        '```',
+        `yarn add --dev ${project.package.packageName}`,
+        '',
+        '# or',
+        '',
+        `npm install ${project.package.packageName} --save-dev`,
+        '```',
+        '',
+        '## Usage',
+        '<!-- TODO: add Usage & Examples -->',
+        '',
+        '## Opening Issues',
+        '',
+        'If you encounter a bug with this package, we want to hear about it. Before opening a new issue, search the existing issues to avoid duplicates.',
+        '',
+        'When opening an issue, include the Kikoda Construct Library version, Node version, and stack trace if available. In addition, include the steps to reproduce when appropriate.',
+        '',
+        'You can also open an issue for a feature request.',
+        '',
+        '## Contributing',
+        '',
+        'If you find an issue with this package and have a fix, please feel free to open a pull request following the [procedures](CONTRIBUTING.md).',
+        '',
+        '## Testing',
+        '',
+        'If you contribute to this package you can run the tests using `yarn test`.',
+        '',
+        '## License',
+        '',
+        'Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.',
+        '',
+        'This product includes software developed at Kikoda (https://www.kikoda.com). Copyright 2022 Kikoda, LLC.',
+      ].join('\n'),
+    });
   }
 }
